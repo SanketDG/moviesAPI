8962183e7e35286bad8a5b9ec3dc9411c630f76a
Sanket Dasgupta <sanketdasgupta@gmail.com>
2020-12-10 19:54:16 +0530

    feat: Add search endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from api.views import (
     MovieListCreateView,
-    MovieDetailView
+    MovieDetailView,
+    search_movies
 )
 from django.urls import include, path
 
 urlpatterns = [
     path("movies/", MovieListCreateView.as_view(), name="movies"),
+    path("movies/search/", search_movies, name="search_movie"),
     path("movies/<pk>/", MovieDetailView.as_view(), name="movie"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
-from rest_framework import generics, pagination
+from django.db.models import Q
+from rest_framework import generics, pagination, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from api.models import Movie
 from api.serializers import MovieSerializer, MovieCreateSerializer
 from api.permissions import IsAdminOrReadOnly
+
 class MovieListCreateView(generics.ListCreateAPIView):
     """
     This is the view that is responsible for listing and creating movies
@@ -27,3 +32,22 @@ class MovieDetailView(generics.RetrieveUpdateDestroyAPIView):
         if self.request.method == "PUT" or self.request.method == "PATCH":
             return MovieCreateSerializer
         return MovieSerializer
+
+@api_view(["GET"])
+def search_movies(request):
+    """
+    Search movies by query on name, director or genre
+    """
+    search_query = request.GET.get("q", None)
+    print(search_query)
+    if search_query is not None:
+        print(search_query)
+        search_results = Movie.objects.filter(
+            Q(name__icontains=search_query)
+            | Q(director__icontains=search_query)
+            | Q(genre__name__icontains=search_query)
+        )
+
+        search_serialized = MovieSerializer(search_results, many=True)
+        return Response(search_serialized.data, status=status.HTTP_200_OK)
+    return Response({"message": "Search query missing"}, status=status.HTTP_200_OK)
