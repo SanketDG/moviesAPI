2c70403e4f16d041afb7e741cb9bfec92feed785
Sanket Dasgupta <sanketdasgupta@gmail.com>
2020-12-10 18:02:18 +0530

    feat: Add Movie and Genre model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    """Genre Model for storing a Genre string"""
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    """Movies model to store a Movie object"""
+
+    name = models.CharField(max_length=200)
+    genre = models.ManyToManyField(Genre)
+    director = models.CharField(max_length=200)
+    imdb_score = models.FloatField()
+    popularity = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} - {self.director} - {self.imdb_score}"
